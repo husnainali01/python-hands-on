1f0724d2cbb4245c10d019cdb2a947a7142a729b
Husnain <husnainali6340@gmail.com>
2021-08-04 02:56:51 +0500

    fix indentation errors

--- prime.py
+++ prime.py
@@ -1,19 +1,24 @@
-isprime = True
-num = input("Enter the Number:")
-num = int(num)
-if num == 0 or num == 1:
-  print(num,'is a prime number')
-else:
-   if num > 1:
-      for i in range(2,num/2):
-         if (num % i) == 0:
-             isprime = False
-             break  
-         else:
-             isprime = True
-   else:
-         print(You are enter the negative number ... please ..  Enter the Positive Number:')
-if isprime == True:
-  print(num,'is a prime number')
-else: 
-  print(num,'is not prime number')
+def findPrimeNumber(num):
+    isprime = True
+    if num == 0 or num == 1:
+        print(num, 'is a prime number')
+    else:
+        if num > 1:
+            for i in range(2, int(num / 2)):
+                if (num % i) == 0:
+                    isprime = False
+                    break
+                else:
+                    isprime = True
+        else:
+            print('You are enter the negative number ... please ..  Enter the Positive Number:')
+    if isprime == True:
+        print(num, 'is a prime number')
+    else:
+        print(num, 'is not prime number')
+
+
+if __name__ == "__main__":
+    num = input("Enter the Number:")
+    num = int(num)
+    findPrimeNumber(num)
